573b40ad3b69597ad9849bdde6691b66a51db084
Brandon Hunter <brandonhunter@protonmail.com>
2019-10-07 10:13:52 +0100

    Add files via upload

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("You get 3 guesses. Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }else
+                {
+                    outOfGuesses = true;
+                }
+                
+             }
+            if(outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                {
+                    Console.Write("You win!");
+                }
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
